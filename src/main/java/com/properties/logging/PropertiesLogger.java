7aed216cd7863f4dd0179b65983d0c7a3e6a3f71
ryan.k.mcdonald <ryan.k.mcdonald@accenture.com>
2015-03-04 16:10:15 -0500

    Update

--- src/main/java/com/properties/logging/PropertiesLogger.java
+++ src/main/java/com/properties/logging/PropertiesLogger.java
@@ -6,10 +6,8 @@ package com.properties.logging;
 import java.lang.reflect.Method;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -18,7 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-//import org.springframework.util.StopWatch;
+// import org.springframework.util.StopWatch;
 
 /**
  * @author ryanmcdonald
@@ -29,109 +27,109 @@ import org.springframework.stereotype.Component;
 @Component
 public class PropertiesLogger {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(PropertiesLogger.class);
+    private static final Logger logger = LoggerFactory
+                                               .getLogger(PropertiesLogger.class);
 
-	@Pointcut("within(com.properties..*)")
-	private void logAll() {
-	}
+    @Pointcut("within(com.properties..*)")
+    private void logAll() {
+    }
 
-	/**
+    /**
+     * 
+     * @param point
+     */
+    @Before("logAll()")
+    public void logBefore(JoinPoint point) {
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        Method method = signature.getMethod();
+        logger.debug(method.toGenericString());
+    }
+
+    /**
+     * 
+     * @param point
+     */
+    @After("logAll()")
+    public void logAfter(JoinPoint point) {
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        Method method = signature.getMethod();
+        logger.debug(method.toGenericString());
+    }
+
+    /**
+     * 
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
+    // @Around("execution(* com.properties.fileio.*.*(..))")
+    // public Object logMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+    // final Logger logger = LoggerFactory.getLogger(joinPoint.getTarget()
+    // .getClass().getName());
+    // Object retVal = null;
+    //
+    // try {
+    // StringBuffer startMessageStringBuffer = new StringBuffer();
+    //
+    // startMessageStringBuffer.append("Start method ");
+    // startMessageStringBuffer.append(joinPoint.getSignature().getName());
+    // startMessageStringBuffer.append("(");
+    //
+    // Object[] args = joinPoint.getArgs();
+    // for (int i = 0; i < args.length; i++) {
+    // startMessageStringBuffer.append(args[i]).append(",");
+    // }
+    // if (args.length > 0) {
+    // startMessageStringBuffer.deleteCharAt(startMessageStringBuffer
+    // .length() - 1);
+    // }
+    //
+    // startMessageStringBuffer.append(")");
+    //
+    // logger.trace(startMessageStringBuffer.toString());
+    //
+    // StopWatch stopWatch = new StopWatch();
+    // stopWatch.start();
+    //
+    // retVal = joinPoint.proceed();
+    //
+    // stopWatch.stop();
+    //
+    // StringBuffer endMessageStringBuffer = new StringBuffer();
+    // endMessageStringBuffer.append("Finish method ");
+    // endMessageStringBuffer.append(joinPoint.getSignature().getName());
+    // endMessageStringBuffer.append("(..); execution time: ");
+    // endMessageStringBuffer.append(stopWatch.getTotalTimeMillis());
+    // endMessageStringBuffer.append(" ms;");
+    //
+    // logger.trace(endMessageStringBuffer.toString());
+    // } catch (Throwable ex) {
+    // StringBuffer errorMessageStringBuffer = new StringBuffer();
+    //
+    // // Create error message
+    // logger.error(errorMessageStringBuffer.toString(), ex);
+    //
+    // throw ex;
+    // }
+    //
+    // return retVal;
+    // }
+
+    /**
 	 * 
-	 * @param point
 	 */
-	@Before("logAll()")
-	public void logBefore(JoinPoint point) {
-		MethodSignature signature = (MethodSignature) point.getSignature();
-		Method method = signature.getMethod();
-		logger.debug(method.toGenericString());
-	}
+    @Pointcut("within(com.properties..*)|| execution(* java.util.concurrent.Callable+.call())|| execution(* java.lang.Runnable+.run())")
+    private void logErrors() {
+    }
 
-	/**
-	 * 
-	 * @param point
-	 */
-	@After("logAll()")
-	public void logAfter(JoinPoint point) {
-		MethodSignature signature = (MethodSignature) point.getSignature();
-		Method method = signature.getMethod();
-		logger.debug(method.toGenericString());
-	}
+    /**
+     * 
+     * @param ex
+     */
+    @AfterThrowing(pointcut = "logErrors()", throwing = "ex")
+    public void logUncaughtException(Exception ex) {
+        logger.error("@EXCEPTION: " + ex.getMessage().toUpperCase());
 
-	/**
-	 * 
-	 * @param joinPoint
-	 * @return
-	 * @throws Throwable
-	 */
-	// @Around("execution(* com.properties.fileio.*.*(..))")
-	// public Object logMethod(ProceedingJoinPoint joinPoint) throws Throwable {
-	// final Logger logger = LoggerFactory.getLogger(joinPoint.getTarget()
-	// .getClass().getName());
-	// Object retVal = null;
-	//
-	// try {
-	// StringBuffer startMessageStringBuffer = new StringBuffer();
-	//
-	// startMessageStringBuffer.append("Start method ");
-	// startMessageStringBuffer.append(joinPoint.getSignature().getName());
-	// startMessageStringBuffer.append("(");
-	//
-	// Object[] args = joinPoint.getArgs();
-	// for (int i = 0; i < args.length; i++) {
-	// startMessageStringBuffer.append(args[i]).append(",");
-	// }
-	// if (args.length > 0) {
-	// startMessageStringBuffer.deleteCharAt(startMessageStringBuffer
-	// .length() - 1);
-	// }
-	//
-	// startMessageStringBuffer.append(")");
-	//
-	// logger.trace(startMessageStringBuffer.toString());
-	//
-	// StopWatch stopWatch = new StopWatch();
-	// stopWatch.start();
-	//
-	// retVal = joinPoint.proceed();
-	//
-	// stopWatch.stop();
-	//
-	// StringBuffer endMessageStringBuffer = new StringBuffer();
-	// endMessageStringBuffer.append("Finish method ");
-	// endMessageStringBuffer.append(joinPoint.getSignature().getName());
-	// endMessageStringBuffer.append("(..); execution time: ");
-	// endMessageStringBuffer.append(stopWatch.getTotalTimeMillis());
-	// endMessageStringBuffer.append(" ms;");
-	//
-	// logger.trace(endMessageStringBuffer.toString());
-	// } catch (Throwable ex) {
-	// StringBuffer errorMessageStringBuffer = new StringBuffer();
-	//
-	// // Create error message
-	// logger.error(errorMessageStringBuffer.toString(), ex);
-	//
-	// throw ex;
-	// }
-	//
-	// return retVal;
-	// }
-
-	/**
-	 * 
-	 */
-	@Pointcut("within(com.properties..*)|| execution(* java.util.concurrent.Callable+.call())|| execution(* java.lang.Runnable+.run())")
-	private void logErrors() {
-	}
-
-	/**
-	 * 
-	 * @param ex
-	 */
-	@AfterThrowing(pointcut = "logErrors()", throwing = "ex")
-	public void logUncaughtException(Exception ex) {
-		logger.error("@EXCEPTION: " + ex.getMessage().toUpperCase());
-
-	}
+    }
 
 }
